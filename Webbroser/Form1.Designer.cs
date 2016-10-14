8012d4ce010417fd3fed19d9db1988d669481e86
Pvedrine <vedrine.p@gmail.com>
2016-10-14 10:26:43 +0100

    First Commit

---
+++ Webbroser/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Webbroser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setAsFavoriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setAsHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinkToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.favoritesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setAsFavoriteToolStripMenuItem,
+            this.setAsHomeToolStripMenuItem,
+            this.changeColourToolStripMenuItem});
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.homeToolStripMenuItem.Text = "File";
+            this.homeToolStripMenuItem.Click += new System.EventHandler(this.homeToolStripMenuItem_Click);
+            // 
+            // setAsFavoriteToolStripMenuItem
+            // 
+            this.setAsFavoriteToolStripMenuItem.Name = "setAsFavoriteToolStripMenuItem";
+            this.setAsFavoriteToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.setAsFavoriteToolStripMenuItem.Text = "Set As Favorite";
+            this.setAsFavoriteToolStripMenuItem.Click += new System.EventHandler(this.setAsFavoriteToolStripMenuItem_Click);
+            // 
+            // setAsHomeToolStripMenuItem
+            // 
+            this.setAsHomeToolStripMenuItem.Name = "setAsHomeToolStripMenuItem";
+            this.setAsHomeToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.setAsHomeToolStripMenuItem.Text = "Set As Home";
+            this.setAsHomeToolStripMenuItem.Click += new System.EventHandler(this.setAsHomeToolStripMenuItem_Click);
+            // 
+            // changeColourToolStripMenuItem
+            // 
+            this.changeColourToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pinkToolStripMenuItem,
+            this.darkToolStripMenuItem,
+            this.pinkToolStripMenuItem1,
+            this.blueToolStripMenuItem,
+            this.redToolStripMenuItem});
+            this.changeColourToolStripMenuItem.Name = "changeColourToolStripMenuItem";
+            this.changeColourToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.changeColourToolStripMenuItem.Text = "Change Colour";
+            this.changeColourToolStripMenuItem.Click += new System.EventHandler(this.changeColourToolStripMenuItem_Click);
+            // 
+            // pinkToolStripMenuItem
+            // 
+            this.pinkToolStripMenuItem.Name = "pinkToolStripMenuItem";
+            this.pinkToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pinkToolStripMenuItem.Text = "White";
+            this.pinkToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // darkToolStripMenuItem
+            // 
+            this.darkToolStripMenuItem.Name = "darkToolStripMenuItem";
+            this.darkToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.darkToolStripMenuItem.Text = "Dark";
+            this.darkToolStripMenuItem.Click += new System.EventHandler(this.darkToolStripMenuItem_Click);
+            // 
+            // pinkToolStripMenuItem1
+            // 
+            this.pinkToolStripMenuItem1.Name = "pinkToolStripMenuItem1";
+            this.pinkToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.pinkToolStripMenuItem1.Text = "Pink";
+            this.pinkToolStripMenuItem1.Click += new System.EventHandler(this.pinkToolStripMenuItem1_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // favoritesToolStripMenuItem
+            // 
+            this.favoritesToolStripMenuItem.Name = "favoritesToolStripMenuItem";
+            this.favoritesToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.favoritesToolStripMenuItem.Text = "Favorites";
+            this.favoritesToolStripMenuItem.Click += new System.EventHandler(this.favoritesToolStripMenuItem_Click);
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.historyToolStripMenuItem.Text = "History";
+            this.historyToolStripMenuItem.Click += new System.EventHandler(this.historyToolStripMenuItem_Click);
+            // 
+            // homeToolStripMenuItem1
+            // 
+            this.homeToolStripMenuItem1.Name = "homeToolStripMenuItem1";
+            this.homeToolStripMenuItem1.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem1.Text = "Home";
+            this.homeToolStripMenuItem1.Click += new System.EventHandler(this.homeToolStripMenuItem1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.favoritesToolStripMenuItem,
+            this.historyToolStripMenuItem,
+            this.homeToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(875, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(714, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-->";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(657, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "<--";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(15, 56);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(845, 452);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(19, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(632, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // goButton
+            // 
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goButton.Location = new System.Drawing.Point(776, 26);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(84, 24);
+            this.goButton.TabIndex = 10;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 535);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setAsFavoriteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem favoritesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem setAsHomeToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ToolStripMenuItem changeColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinkToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+    }
+}
+
---
+++ Webbroser/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webbroser
+{
+    public partial class Form1 : Form
+    {
+
+        /*TO DO
+         * Supporter erreurs 400, 403 et 404
+         * Modifier les fav (supprimer)
+         * charger l'historique au démarrage
+         * multi thread pour avoir plusieurs pages
+         * COMMENTER
+         */
+        Uri[] Url = new Uri[20];
+        int index = 0;
+        int fav = 0;
+        ToolStripMenuItem[] history = new ToolStripMenuItem[50];
+        // List<ToolStripMenuItem> history = new List<ToolStripMenuItem>();
+        ToolStripMenuItem[] favorites = new ToolStripMenuItem[50];
+        string FileFavorites = @"C:\Users\Public\TestFolder\Favorites.txt";
+        string FileHistory = @"C:\Users\Public\TestFolder\History.txt";
+
+        public void GetPage(string s)
+        {
+            try
+            {
+                if (!s.Equals(""))
+                {
+                    Url[index] = new Uri(s);
+                    //We get the Http text from the URL
+                    textBox2.Text = Encoding.UTF8.GetString(Encoding.Default.GetBytes((new WebClient().DownloadString(Url[index]))));
+                    textBox1.Text = Url[index].ToString();
+                    //We add the URL onto the history
+                  //  History(Url[index].ToString());
+                    index++;
+                    if (index == 20)
+                    {
+                        index = 0;
+                    }
+
+                }
+            }
+
+            //Handling errors
+            
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if(resp.StatusCode == HttpStatusCode.NotFound) //404
+                    {
+                        MessageBox.Show("Error, 404 Not Found", "Error 404", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if(resp.StatusCode == HttpStatusCode.BadRequest)//400
+                    {
+                        MessageBox.Show("Error, 400 Bad Request", "Error 400", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    if (resp.StatusCode == HttpStatusCode.Forbidden)//403
+                    {
+                        MessageBox.Show("Error, 403 Forbidden", "Error 403", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                   }
+               
+            }
+        
+            
+        }
+        //Previous function 
+        public void Previous()
+        {
+            if (index >0)
+            {
+                GetPage(Url[index-1].ToString());
+                index -= 2;
+               
+            }
+        }
+        //forward function
+        public void Forward()
+        {
+            if (Url[index+1] != null)
+            {
+                GetPage(Url[index + 1].ToString());
+                    
+            }
+        }
+
+        public void History(string s)
+        {
+            if (Url[index] != null )
+                {
+                    history[index] = new ToolStripMenuItem();
+                    history[index].Text =  s;
+                   // history[index].Name = Url[index].ToString();
+                    // history[index].ToolTipText = Url[index].ToString();
+                    history[index].Click += new EventHandler(History_click);
+                   if (index == 0 || history[index] != null)
+                    historyToolStripMenuItem.DropDownItems.Add(history[index]);
+            }
+               
+
+            }
+            
+           public void SetToFavorites(string s)
+        {
+            if (s != null && !checkfav(s))
+            {
+                favorites[fav] = new ToolStripMenuItem();
+                favorites[fav].Text = s;
+                favorites[fav].Name = s;
+                favorites[fav].Click += new EventHandler(Favorites_Click);
+                if (fav == 0 || favorites[fav] != null)
+                {
+                    favoritesToolStripMenuItem.DropDownItems.Add(favorites[fav]);
+                }
+            }
+            fav++;
+
+        }
+        public bool checkfav(string s)
+        {
+            bool test = false;
+            for (int i = 0; i < favorites.Length; i++)
+            {
+                if (favorites[i] != null)
+                {
+                    test = favorites[i].Text == s ? true : false;
+                }
+                if (test)
+                {
+                    break;
+                }
+            }
+            return test;
+        }
+        public void Get_Fav(string file)
+        {
+            string line;
+            if (File.Exists(file))
+            {
+                System.IO.StreamReader filetmp = new System.IO.StreamReader(file);
+
+                if (file == FileHistory)
+                {
+                    while ((line = filetmp.ReadLine()) != null)
+                        History(line);
+                }
+
+                else
+
+                while ((line = filetmp.ReadLine()) != null)
+                {
+                    SetToFavorites(line);
+                    
+                }
+                filetmp.Close();
+
+            }
+            //if the favorites files doesn't exist, we create it
+            else
+            {
+                File.Create(file);
+            }
+        }
+
+        public void WriteFile(string file, ToolStripMenuItem[] tab)
+        {
+            if (File.Exists(file))
+            {
+                for (int i = 0; i < tab.Length; i++)
+                {
+                    if (tab[i] != null)
+                    {
+                        System.IO.File.WriteAllText(file, tab[i].Text);
+                    }
+                }
+            }
+        }
+
+        private void Favorites_Click (object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedFavorites = (ToolStripMenuItem)sender;
+            GetPage(clickedFavorites.Text);
+        }
+
+        private void History_click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedHistory = (ToolStripMenuItem)sender;
+            GetPage(clickedHistory.Text);
+        }
+        private void MenuItemClickHandler(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+     
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetPage(textBox1.Text);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Forward();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Previous();
+        }
+
+       
+
+        private void homeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.home != null)
+            GetPage(Properties.Settings.Default.home);
+        }
+
+        private void setAsHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.home = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+
+       
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //We get the Home Page URL loaded
+            GetPage(Properties.Settings.Default.home);
+            //We get the favorites
+            Get_Fav(FileFavorites);
+            Get_Fav(FileHistory);
+        }
+
+        private void setAsFavoriteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetToFavorites(Url[index -1].ToString());
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //once the application closes, we write all the favorites/history in a text file
+            WriteFile(FileFavorites, favorites);
+            WriteFile(FileHistory, history);              
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetPage(textBox1.Text);
+        }
+
+
+        private void ColourMode(System.Drawing.Color fore, System.Drawing.Color back)
+        {
+            menuStrip1.BackColor = back;
+            menuStrip1.ForeColor = fore;
+            textBox1.BackColor = back;
+            textBox1.ForeColor = fore;
+            textBox2.BackColor = back;
+            textBox2.ForeColor = fore;
+            BackColor = back;
+            ForeColor = fore;
+            button1.BackColor = back;
+            button1.ForeColor = fore;
+            button2.ForeColor = fore;
+            button2.BackColor = back;
+            goButton.ForeColor = fore;
+            goButton.BackColor = back;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void historyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip3_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void favoritesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+       
+        
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeColourToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pinkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColourMode(System.Drawing.Color.Black, System.Drawing.Color.White);
+        }
+
+        private void darkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColourMode(System.Drawing.Color.White, System.Drawing.Color.Black);
+
+        }
+
+        private void pinkToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ColourMode(System.Drawing.Color.Black, System.Drawing.Color.LightPink);
+
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColourMode(System.Drawing.Color.LightGreen, System.Drawing.Color.Blue);
+
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColourMode(System.Drawing.Color.Blue, System.Drawing.Color.Red);
+
+        }
+    }
+}
